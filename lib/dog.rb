27e5a2af63971d7647c22a1ea4f207710d1ecd9e
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-18 17:53:57 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog 
+  
+  def name=(dog_name)
+    @this_dog_name = dog_name
+  end
+  
+  def name 
+    @this_dog_name
+  end
+  
+  def breed=(dog_breed)
+    @this_breed = dog_breed
+  end
+  
+  def breed
+    @this_breed
+  end
+  
+  
+end
